130bd2ee9b6131d271c527033151d3fd23b80549
Ivaylo Stoyanov <me@ivkos.com>
2016-11-07 18:52:48 +0200

    Configuration: Update error message

--- src/util/Configuration.js
+++ src/util/Configuration.js
@@ -6,18 +6,21 @@ import Bro from "brototype";
 @Singleton
 export default class Configuration {
     constructor(path) {
-        if (!fs.existsSync(path)) {
-            throw new Error("Configuration file '" + path + "' does not exist.");
+        this.configFilePath = path;
+
+        if (!fs.existsSync(this.configFilePath)) {
+            throw new Error("Configuration file '" + this.configFilePath + "' does not exist.");
         }
 
-        const configurationHolder = YAML.parse(fs.readFileSync(path, 'utf8'));
+        const configurationHolder = YAML.parse(fs.readFileSync(this.configFilePath, 'utf8'));
 
         this.bro = Bro(configurationHolder);
     }
 
     get(property) {
         if (this.bro.doYouEven(property) === Bro.NOWAY) {
-            throw new Error("Configuration property '" + property + "' was not found.");
+            throw new Error("Configuration property '" + property + "' " +
+                "was not found in configuration file '" + this.configFilePath + "'");
         }
 
         return this.bro.iCanHaz(property);
