3d000334249ef1e42d41a7ae6335ce54e7f23fcf
Ivaylo Stoyanov <me@ivkos.com>
2016-11-14 22:28:09 +0200

    Add similarity calculation for aliases and scheduled updating of participants

---
+++ src/modules/scheduled-tasks/ThreadParticipantsUpdater.js
@@ -0,0 +1,58 @@
+import { dependencies as Inject, singleton as Singleton } from "needlepoint";
+import ScheduledTask from "../ScheduledTask";
+import Threads from "../../util/Threads";
+import ChatApi from "../../util/ChatApi";
+import Promise from "bluebird";
+
+@Singleton
+@Inject(Threads, ChatApi)
+export default class ThreadParticipantsUpdater extends ScheduledTask {
+    /**
+     * @param {Threads} threads
+     * @param {ChatApi} api
+     */
+    constructor(threads, api) {
+        super();
+
+        this.threads = threads;
+        this.api = api;
+    }
+
+    getInterval() {
+        return 1 * 60 * 1000;
+    }
+
+    shouldExecuteOnStart() {
+        return true;
+    }
+
+    execute() {
+        const threadInfosPromise = this.threads
+            .getThreadIds()
+            .map(id => this.api.getThreadInfo(id).then(info => {
+                info._threadId = id;
+                return info;
+            }));
+
+        return Promise.all(threadInfosPromise)
+            .then(threadInfos => threadInfos.map(info => {
+                const threadId = info._threadId;
+
+                if (info.nicknames && info.nicknames.length > 0) {
+                    Object.keys(info.nicknames).forEach(userId =>
+                        this.threads.setAlias(threadId, parseInt(userId), info.nicknames[userId])
+                    );
+                }
+
+                return this.api.getUserInfo(info.participantIDs)
+                    .then(info => Object.keys(info).map(k => [k, info[k]]).forEach(([userId, userInfo]) => {
+                        [userInfo.name, userInfo.vanity]
+                            .filter(alias => alias && alias.length > 0)
+                            .forEach(alias => {
+                                this.threads.setAlias(threadId, parseInt(userId), alias);
+                            })
+                    }))
+            }))
+            .all();
+    }
+}
