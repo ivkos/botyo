ce3e589d4aab51cbb9d8a1ac7eaea40b039eeff4
Ivaylo Stoyanov <me@ivkos.com>
2016-11-14 17:45:48 +0200

    Automatically accept message requests for threadIds in configuration

---
+++ src/modules/scheduled-tasks/MessageRequestAcceptor.js
@@ -0,0 +1,33 @@
+import { dependencies as Inject } from "needlepoint";
+import ScheduledTask from "../ScheduledTask";
+import Threads from "../../util/Threads";
+import ChatApi from "../../util/ChatApi";
+import Promise from "bluebird";
+
+@Inject(ChatApi, Threads)
+export default class MessageRequestAcceptor extends ScheduledTask {
+    /**
+     * @param {ChatApi} api
+     * @param {Threads} threads
+     */
+    constructor(api, threads) {
+        super();
+
+        this.api = api;
+        this.threads = threads;
+    }
+
+    getInterval() {
+        return 5 * 60 * 1000;
+    }
+
+    shouldExecuteOnStart() {
+        return true;
+    }
+
+    execute() {
+        const promises = this.threads.getThreadIds().map(id => this.api.handleMessageRequest(id, true));
+
+        return Promise.all(promises);
+    }
+}
