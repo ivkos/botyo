99ba9674914fe197a2fd772a39b45dc419d61ca9
Ivaylo Stoyanov <me@ivkos.com>
2016-11-15 19:21:08 +0200

    Simplify run script

--- scripts/build-1-node-deps.sh
+++ scripts/build-1-node-deps.sh
@@ -4,9 +4,6 @@ set -e
 
 cd `dirname $0`
 
-if !(docker images | grep ivkos/facebook-group-chat-bot-backbone)
-then
-    ./build-0-backbone.sh
-fi
+./build-0-backbone.sh
 
 docker build -t ivkos/facebook-group-chat-bot-node-deps -f 1-node-deps.dockerfile ../
--- scripts/build-2-app.sh
+++ scripts/build-2-app.sh
@@ -4,9 +4,6 @@ set -e
 
 cd `dirname $0`
 
-if !(docker images | grep ivkos/facebook-group-chat-bot-node-deps)
-then
-    ./build-1-node-deps.sh
-fi
+./build-1-node-deps.sh
 
 docker build -t ivkos/facebook-group-chat-bot-app -f 2-app.dockerfile ../
